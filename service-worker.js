c11375995a2f5eddaedb8c3d5aeeef6771114b49
reshmi4chat-ui <reshmi4chat@gmail.com>
2026-03-25 13:58:42 -0700

    Add service worker for caching resources

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("calc-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./app.js",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
